f64f0288f9f1ffa13dcba8c4870376a1f03ffbf4
Rob Waller <r.t.waller@gmail.com>
2020-06-23 10:26:23 -0500

    Updated the map to be a generic map

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,13 @@
   <v-app>
     <v-toolbar app color="rgb(55, 71, 79)">
       <v-toolbar-title class="headline text-uppercase">
-        <!-- <span>Chicago:</span>
-        <span class="font-weight-light ml-2">Real Estate</span>-->
-        <img
+        <span style="color: white">Chicago:</span>
+        <span style="color: white" class="font-weight-light ml-2">Real Estate</span>
+        <!-- <img
           src="https://www.enodoinc.com/assets/images/logo/logo_green.svg"
           style="height: 30px;"
           alt="Enodo"
-        />
+        />-->
       </v-toolbar-title>
     </v-toolbar>
 
